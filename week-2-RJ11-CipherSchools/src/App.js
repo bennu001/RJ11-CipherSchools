46ac64449dac2e409dd0c5081db8114fc6b91a6d
Bennu Sai <121197282+bennu001@users.noreply.github.com>
2023-02-04 20:01:53 +0530

    week 2 uploded

---
+++ week-2-RJ11-CipherSchools/src/App.js
@@ -0,0 +1,47 @@
+// import react, { useState } from "react"
+
+
+import React from 'react'
+import './App.css';
+
+// function App() {
+//   // const [h1Text, setH1Test] = useState("");
+//   return (
+//     <div className="App">
+//       <h1>Week2 </h1>
+//       <h1>hello</h1>
+//       <button onClick={(e) => {
+//         // setH1Text("This is gandu");
+//       }}
+
+//       >
+//         Click Me </button>
+//     </div>
+//   );
+// }
+// function App() {
+//   function handleSubmit(e) {
+//     e.preventDefault();
+//     console.log('You clicked submit.');
+//   }
+
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <button type="submit">Submit</button>
+//     </form>
+//   );
+// }
+
+// export default App;
+
+
+function App() {
+  return (
+    <div>
+      <app />
+
+    </div>
+  )
+}
+
+export default App
---
+++ week-2-RJ11-CipherSchools/src/components/Name.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function name() {
+    return (
+        <div>name</div>
+    )
+}
+
+
+export default name
